b2c2456a635c26b755d7da9623f7068bde45f85d
GaryFernandoYM <miraclevann224@gmail.com>
2023-09-22 19:17:45 -0500

    ventas

--- ms-ventas/src/main/java/com/example/msventas/MsVentasApplication.java
+++ ms-ventas/src/main/java/com/example/msventas/MsVentasApplication.java
@@ -16,9 +16,9 @@ public class MsVentasApplication {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(new Info()
-                .title("OPEN API MICROSERVICIO CATALOGO")
+                .title("OPEN API MICROSERVICIO VENTAS")
                 .version("0.0.1")
-                .description("servicios web catalogo")
+                .description("servicios web ventas")
                 .termsOfService("http://swagger.io/terms")
                 .license(new License().name("Apache 2.0").url("http://springdoc.org"))
 
