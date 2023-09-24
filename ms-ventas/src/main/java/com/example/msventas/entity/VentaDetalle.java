96e3dc55e5d05e569b8c9888261de0f4a78273f2
guver71 <gccoarite@gmail.com>
2023-09-24 18:58:23 -0500

    Description VentaDetalle

--- ms-ventas/src/main/java/com/example/msventas/entity/VentaDetalle.java
+++ ms-ventas/src/main/java/com/example/msventas/entity/VentaDetalle.java
@@ -1,4 +1,27 @@
 package com.example.msventas.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class VentaDetalle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Double cantidad;
+    private Double precioU;
+    private Double subtotal;
+    private Double total_factura;
+    private Integer productoId;
+
+    public VentaDetalle(){
+        this.cantidad = (double) 0;
+        this.precioU = (double) 0;
+        this.subtotal = (double) 0;
+        this.total_factura = (double) 0;
+    }
 }
