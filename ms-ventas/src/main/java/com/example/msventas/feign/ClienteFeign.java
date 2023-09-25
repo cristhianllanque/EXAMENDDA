b8f4fe3445c49c9fc0041f880a9cd31bc6207e25
guver71 <gccoarite@gmail.com>
2023-09-25 08:01:07 -0500

    Completado

--- ms-ventas/src/main/java/com/example/msventas/feign/ClienteFeign.java
+++ ms-ventas/src/main/java/com/example/msventas/feign/ClienteFeign.java
@@ -1,6 +1,8 @@
 package com.example.msventas.feign;
 
+import com.example.msventas.dto.Cliente;
 import com.example.msventas.dto.Producto;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,5 +10,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-cliente-service", path = "/cliente")
 public interface ClienteFeign {
     @GetMapping("/{id}")
-    public ResponseEntity<Producto> listById(@PathVariable(required = true) Integer id);
+    public ResponseEntity<Cliente> listById(@PathVariable(required = true) Integer id);
 }
