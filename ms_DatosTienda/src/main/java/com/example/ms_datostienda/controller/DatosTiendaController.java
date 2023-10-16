dd7c1d25e95657d131f64a7a76f50a331346eb90
brayner573 <braynermamani@upeu.edu.pe>
2023-10-16 10:06:02 -0500

    brayner45677

--- ms_DatosTienda/src/main/java/com/example/ms_datostienda/controller/DatosTiendaController.java
+++ ms_DatosTienda/src/main/java/com/example/ms_datostienda/controller/DatosTiendaController.java
@@ -2,6 +2,7 @@ package com.example.ms_datostienda.controller;
 
 import com.example.ms_datostienda.entity.DatosTienda;
 import com.example.ms_datostienda.service.DatosTiendaService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +23,12 @@ public class DatosTiendaController {
         return  ResponseEntity.ok(datosTiendaService.guardar(datosTienda));
     }
     @PutMapping()
+
     public ResponseEntity<DatosTienda> update(@RequestBody DatosTienda datosTienda){
         return ResponseEntity.ok(datosTiendaService.actualizar(datosTienda));
     }
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "ProveedoreslistarPorIdCB", fallbackMethod = "fallBackProveedoreslistarPorIdCB")
     public ResponseEntity<DatosTienda> listById(@PathVariable(required = true)Integer id){
         return ResponseEntity.ok().body(datosTiendaService.listarPorId(id).get());
 
@@ -38,5 +41,11 @@ public class DatosTiendaController {
 
     }
 
+    private ResponseEntity<Producto> fallBackProductoListarPorIdCB(@PathVariable(required = true) Integer id, RuntimeException e) {
+        Producto producto = new Producto();
+        producto.setId(90000);
+        return ResponseEntity.ok().body(producto);
+
+    }
 
 }
