ce8f22415b83f467fb43198fc46cce7c576766ee
cristhianllanque <cristhianllanque65@gmail.com>
2023-10-15 21:29:53 -0500

    resilenciafin

--- ms-productos/src/main/java/com/example/msproductos/controller/ProductoController.java
+++ ms-productos/src/main/java/com/example/msproductos/controller/ProductoController.java
@@ -14,28 +14,37 @@ import java.util.List;
 public class ProductoController {
     @Autowired
     private ProductoService productoService;
+
     @GetMapping
-    public ResponseEntity<List<Producto>> list(){ return ResponseEntity.ok().body(productoService.listar());}
-    @PostMapping
-    public ResponseEntity<Producto> save(@RequestBody Producto producto){return ResponseEntity.ok(productoService.guardar(producto));}
-    @PostMapping
-    public ResponseEntity<Producto> update(@RequestBody Producto producto){return ResponseEntity.ok(productoService.actualizar(producto));
+    public ResponseEntity<List<Producto>> list() {
+        return ResponseEntity.ok().body(productoService.listar());
     }
 
-    @CircuitBreaker(name = "productoListarPorIdCB" , fallbackMethod = "fallBackProductoListarPorIdCB")
-    @GetMapping("{/id}")
-    public ResponseEntity<Producto> listById(@PathVariable(required = true) Integer id){
+    @PostMapping
+    public ResponseEntity<Producto> save(@RequestBody Producto producto) {
+        return ResponseEntity.ok(productoService.guardar(producto));
+    }
+
+    @PutMapping
+    public ResponseEntity<Producto> update(@RequestBody Producto producto) {
+        return ResponseEntity.ok(productoService.actualizar(producto));
+    }
+
+    @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallBackProductoListarPorIdCB")
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> listById(@PathVariable(required = true) Integer id) {
         return ResponseEntity.ok().body(productoService.listarPorId(id).get());
     }
+
     @DeleteMapping("/{id}")
-    public String deleteById(@PathVariable(required = true) Integer id) {productoService.eliminarPorId(id);
-    return "EliminacionCorrecta";
+    public String deleteById(@PathVariable(required = true) Integer id) {
+        productoService.eliminarPorId(id);
+        return "EliminacionCorrecta";
     }
-    private ResponseEntity<Producto> fallBackProductoListarPorIdCB(@PathVariable(required = true) Integer id, RuntimeException e){
+
+    private ResponseEntity<Producto> fallBackProductoListarPorIdCB(@PathVariable(required = true) Integer id, RuntimeException e) {
         Producto producto = new Producto();
         producto.setId(90000);
         return ResponseEntity.ok().body(producto);
     }
-
-
 }
