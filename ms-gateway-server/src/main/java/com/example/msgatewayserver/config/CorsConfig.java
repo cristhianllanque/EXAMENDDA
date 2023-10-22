dafb0cc2fd70fb24c7683808f85169346dc5d50f
GaryFernandoYM <miraclevann224@gmail.com>
2023-10-22 11:30:08 -0500

    CORS

---
+++ ms-gateway-server/src/main/java/com/example/msgatewayserver/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.example.msgatewayserver.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues(); // Permite las configuraciones CORS predeterminadas
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
