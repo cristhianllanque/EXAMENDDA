f28ff4bfc881ea9783eea21b24e441e1d095667e
GaryFernandoYM <miraclevann224@gmail.com>
2023-10-22 14:43:36 -0500

    cors

--- ms-gateway-server/src/main/java/com/example/msgatewayserver/config/CorsConfig.java
+++ ms-gateway-server/src/main/java/com/example/msgatewayserver/config/CorsConfig.java
@@ -1,19 +1,22 @@
 package com.example.msgatewayserver.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @Configuration
 public class CorsConfig {
-    /*@Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.applyPermitDefaultValues(); // Permite las configuraciones CORS predeterminadas
-
+    @Bean
+    public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }*/
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000"); // Permite las solicitudes desde tu aplicación React
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
