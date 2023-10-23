637401401b28325107f9629849220ea2927408e2
cristhianllanque <cristhianllanque65@gmail.com>
2023-10-22 21:07:43 -0500

    cambios

--- ms-productos/src/main/java/com/example/msproductos/entity/Producto.java
+++ ms-productos/src/main/java/com/example/msproductos/entity/Producto.java
@@ -1,13 +1,11 @@
 package com.example.msproductos.entity;
 
-import
-        jakarta.persistence.Entity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-
 import java.math.BigDecimal;
 import java.util.Date;
 
