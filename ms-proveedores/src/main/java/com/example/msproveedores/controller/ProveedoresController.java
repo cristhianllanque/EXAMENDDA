fb88a2ed6364858b7952a9bed179ecb3300aeb3c
GaryFernandoYM <miraclevann224@gmail.com>
2023-10-29 12:02:21 -0500

    cors

--- ms-proveedores/src/main/java/com/example/msproveedores/controller/ProveedoresController.java
+++ ms-proveedores/src/main/java/com/example/msproveedores/controller/ProveedoresController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/proveedores")
 public class ProveedoresController {
