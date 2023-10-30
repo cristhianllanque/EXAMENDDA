5b42f14423b945f272416ec845262c85c1bed3fe
cristhianllanque <cristhianllanque65@gmail.com>
2023-10-29 21:14:52 -0500

    nose

--- ms-opiniones/src/main/java/com/example/msopiniones/MsOpinionesApplication.java
+++ ms-opiniones/src/main/java/com/example/msopiniones/MsOpinionesApplication.java
@@ -1,13 +1,29 @@
 package com.example.msopiniones;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsOpinionesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsOpinionesApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("OPEN API MICROSERVICIO OPINIONES")
+				.version("0.0.1")
+				.description("servicios web opiniones")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+
+		);
 	}
 
 }
