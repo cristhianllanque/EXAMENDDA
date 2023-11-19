e02da4fdbcdf8e890823ff2823401e44bf34b9e1
GaryFernandoYM <miraclevann224@gmail.com>
2023-11-19 18:56:46 -0500

    compras

--- microservicios-web/src/app/views/dashboard/Setup/compr/components/list/compr-list.component.ts
+++ microservicios-web/src/app/views/dashboard/Setup/compr/components/list/compr-list.component.ts
@@ -9,7 +9,7 @@ import {PdfService} from "../../../../../../shared/files";
   selector: 'app-compr-list',
   template: `
     <div class="float-end">
-      <div> compras</div>
+      <div>  detalle </div>
       <button type="button" (click)="generatePDF()" class="btn btn-gm-danger">
         <span class="{{ abcForms.btnNew.icon }} lamb-icon"></span> Generar PDF
       </button>
